dfb4651a3d30698b06bbc9978c67f0653a446d95
Gideonngige <ushindigideon01@gmail.com>
2025-12-18 13:55:11 +0300

    added product id(barcode number)

--- app/(main)/addproduct.jsx
+++ app/(main)/addproduct.jsx
@@ -25,6 +25,18 @@ export default function AddProduct() {
           />
         </View>
 
+        {/* Product ID-barcode number */}
+        <View className="mb-6">
+          <Text className="text-gray-700 mb-2 font-medium">
+            Product Id (barcode number)
+          </Text>
+          <TextInput
+            placeholder="e.g. 1234567891234"
+            className="bg-white rounded-2xl px-5 py-4 text-gray-900 shadow"
+            placeholderTextColor="#9ca3af"
+          />
+        </View>
+
         {/* Price */}
         <View className="mb-6">
           <Text className="text-gray-700 mb-2 font-medium">
